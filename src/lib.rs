c5f4618bfe5ad2cf14c9bdd5b488f6b23d30432b
Linus Färnstrand <faern@faern.net>
2020-05-17 10:44:59 +0200

    New style

--- src/lib.rs
+++ src/lib.rs
@@ -1,180 +1,53 @@
-//! A loom aware async executor.
-//!
-//! This library is an async Rust executor that can be made [loom] aware. This executor is very
-//! simple. It just spawns a thread for each future spawned with the `spawn` function.
-//!
-//! If built without the `loom` cfg enabled this library does not use loom in any way. But when
-//! built with `--cfg loom` it uses primitives from loom to implement the executor. This makes
-//! it possible to write tests for your `Future`s and have loom try all possible
-//!
-//! [loom] https://crates.io/crates/loom
-
-#![feature(thread_id_value)]
-
-use core::cell::RefCell;
-use core::fmt;
-use core::future::Future;
-use core::pin::Pin;
-use core::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
-use core::mem;
-
 #[cfg(loom)]
-use loom as std_or_loom;
+use loom::sync::{Arc, Condvar, Mutex};
 #[cfg(not(loom))]
-use std as std_or_loom;
+use std::sync::{Arc, Condvar, Mutex};
 
-use std_or_loom::sync::atomic::{AtomicBool, Ordering};
-use std_or_loom::sync::{Condvar, Mutex};
-use std_or_loom::thread;
+use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 
-std_or_loom::thread_local!(static RUNTIME: RefCell<Option<*const Runtime>> = Default::default());
+struct Park(Mutex<bool>, Condvar);
 
-pub struct Runtime {
-    threads: Mutex<Vec<thread::JoinHandle<()>>>,
-    cancelled: AtomicBool,
-    condvar: Condvar,
-}
-
-impl Default for Runtime {
+impl Default for Park {
     fn default() -> Self {
-        Self {
-            threads: Mutex::new(Vec::new()),
-            cancelled: AtomicBool::new(false),
-            condvar: Condvar::new(),
-        }
+        Park(Mutex::new(false), Condvar::new())
     }
 }
 
-impl Runtime {
-    pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn block_on<F: Future>(&mut self, future: F) -> F::Output {
-        RUNTIME.with(|rt| rt.replace(Some(self)));
-        let result = run_future(future).expect("Runtime cancelled inside block_on");
-        eprintln!("Cancelling runtime");
-        // Cancel this runtime and wait for all threads to exit.
-        let mut threads = self.threads.lock().unwrap();
-        self.cancelled.store(true, Ordering::SeqCst);
-        self.condvar.notify_all();
-        eprintln!("Joining {} threads", threads.len());
-        for thread in threads.drain(..) {
-            eprintln!("block_on: joining thread");
-            thread.join().unwrap();
-            eprintln!("block_on: thread joined");
-        }
-        result
-    }
+fn unpark(park: &Park) {
+    *park.0.lock().unwrap() = true;
+    park.1.notify_one();
 }
 
-/// Spawns a new asynchronous task, returning a JoinHandle for it.
-///
-/// This function must be called from the context of a loom executor runtime. Tasks running on the
-/// loom executor runtime are always inside its context.
-pub fn spawn<T>(task: T) -> JoinHandle<T::Output>
-where
-    T: Future + Send + 'static,
-    T::Output: Send + 'static,
-{
-    let runtime: &Runtime =
-        RUNTIME.with(|rt| unsafe { &*rt.borrow().expect("Spawn not called from runtime thread") });
-    let (result_sender, result_receiver) = oneshot::channel();
-    // If the runtime is not cancelled, spawn a thread running the task.
-    let mut runtime_threads = runtime.threads.lock().unwrap();
-    if !runtime.cancelled.load(Ordering::SeqCst) {
-        eprintln!("{} spawn: Spawning worker thread", tid());
-        let thread = thread::spawn(move || {
-            eprintln!("{} worker thread: spawned", tid());
-            RUNTIME.with(|rt| rt.replace(Some(runtime)));
-            if let Some(result) = run_future(task) {
-                let _ = result_sender.send(result);
-            }
-            eprintln!("{} worker thread: exiting", tid());
-        });
-        // Insert the new thread's handle into the list
-        runtime_threads.push(thread);
-    }
-    JoinHandle { result_receiver }
-}
-
-pub struct JoinHandle<T> {
-    result_receiver: oneshot::Receiver<T>,
-}
-
-impl<T> Future for JoinHandle<T> {
-    type Output = Result<T, JoinError>;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        Future::poll(Pin::new(&mut self.result_receiver), cx).map_err(|_| JoinError)
-    }
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub struct JoinError;
-
-impl fmt::Display for JoinError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        "runtime cancelled".fmt(f)
-    }
-}
-
-impl std::error::Error for JoinError {}
-
-fn run_future<F: Future>(mut future: F) -> Option<F::Output> {
-    let runtime: &Runtime = RUNTIME.with(|rt| unsafe {
-        &*rt.borrow()
-            .expect("run_future not called from runtime thread")
-    });
-    let waker = unsafe { new_waker(&runtime.condvar) };
-    let mut context = Context::from_waker(&waker);
-    while !runtime.cancelled.load(Ordering::SeqCst) {
-        let pinned_future = unsafe { Pin::new_unchecked(&mut future) };
-        if let Poll::Ready(value) = Future::poll(pinned_future, &mut context) {
-            eprintln!("{} run_future: Returning yielded value", tid());
-            return Some(value);
-        }
-        eprintln!("{} run_future: Locking threads mutex", tid());
-        let threads = runtime.threads.lock().unwrap();
-        eprintln!("{} run_future: Locked threads mutex. Waiting on condvar", tid());
-        mem::drop(runtime.condvar.wait(threads).unwrap());
-        eprintln!("{} run_future: Woke up from condvar wait", tid());
-    }
-    None
-}
-
-unsafe fn new_waker(condvar: &Condvar) -> Waker {
-    Waker::from_raw(RawWaker::new(condvar as *const _ as *const _, &VTABLE))
-}
-
-const VTABLE: RawWakerVTable = RawWakerVTable::new(
-    raw_waker_clone,
-    raw_waker_wake,
-    raw_waker_wake_by_ref,
-    raw_waker_drop,
+static VTABLE: RawWakerVTable = RawWakerVTable::new(
+    |clone_me| unsafe {
+        let arc = Arc::from_raw(clone_me as *const Park);
+        std::mem::forget(arc.clone());
+        RawWaker::new(Arc::into_raw(arc) as *const (), &VTABLE)
+    },
+    |wake_me| unsafe { unpark(&Arc::from_raw(wake_me as *const Park)) },
+    |wake_by_ref_me| unsafe { unpark(&*(wake_by_ref_me as *const Park)) },
+    |drop_me| unsafe { drop(Arc::from_raw(drop_me as *const Park)) },
 );
 
-unsafe fn raw_waker_clone(condvar_ptr: *const ()) -> RawWaker {
-    eprintln!("{} raw_waker_clone", tid());
-    RawWaker::new(condvar_ptr, &VTABLE)
-}
+/// Run a `Future`.
+pub fn block_on<F: std::future::Future>(mut f: F) -> F::Output {
+    let mut f = unsafe { std::pin::Pin::new_unchecked(&mut f) };
+    let park = Arc::new(Park::default());
+    let sender = Arc::into_raw(park.clone());
+    let raw_waker = RawWaker::new(sender as *const _, &VTABLE);
+    let waker = unsafe { Waker::from_raw(raw_waker) };
+    let mut cx = Context::from_waker(&waker);
 
-unsafe fn raw_waker_wake(condvar_ptr: *const ()) {
-    eprintln!("{} raw_waker_wake", tid());
-    let condvar = &*(condvar_ptr as *const Condvar);
-    condvar.notify_all();
-}
-
-unsafe fn raw_waker_wake_by_ref(condvar_ptr: *const ()) {
-    eprintln!("{} raw_waker_wake_by_ref", tid());
-    let condvar = &*(condvar_ptr as *const Condvar);
-    condvar.notify_all();
-}
-
-unsafe fn raw_waker_drop(_condvar_ptr: *const ()) {
-    eprintln!("{} raw_waker_drop", tid());
-}
-
-fn tid() -> u64 {
-    std::thread::current().id().as_u64().get()
+    loop {
+        match f.as_mut().poll(&mut cx) {
+            Poll::Pending => {
+                let mut runnable = park.0.lock().unwrap();
+                while !*runnable {
+                    runnable = park.1.wait(runnable).unwrap();
+                }
+                *runnable = false;
+            }
+            Poll::Ready(val) => return val,
+        }
+    }
 }
--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -1,4 +1,15 @@
-use loom_executor::Runtime;
+use core::pin::Pin;
+use core::task::{Context, Poll};
+
+#[cfg(loom)]
+use loom::{sync::Arc, thread};
+#[cfg(not(loom))]
+use std::{sync::Arc, thread};
+
+#[cfg(not(loom))]
+use core::sync::atomic::{AtomicBool, Ordering};
+#[cfg(loom)]
+use loom::sync::atomic::{AtomicBool, Ordering};
 
 fn maybe_loom_model(test: impl Fn() + Sync + Send + 'static) {
     #[cfg(loom)]
@@ -10,37 +21,57 @@ fn maybe_loom_model(test: impl Fn() + Sync + Send + 'static) {
 #[test]
 fn block_on_unit() {
     maybe_loom_model(|| {
-        let _nothing: () = Runtime::new().block_on(async {});
+        let _nothing: () = loom_executor::block_on(async {});
     })
 }
 
 #[test]
 fn block_on_simple_value() {
     maybe_loom_model(|| {
-        let i: u128 = Runtime::new().block_on(async { 95u128 });
+        let i: u128 = loom_executor::block_on(async { 95u128 });
         assert_eq!(i, 95);
     })
 }
 
 #[test]
-fn block_on_spawn() {
+fn block_on_sleep() {
     maybe_loom_model(|| {
-        eprintln!("==== NEW TEST ====");
-        let i: u128 = Runtime::new().block_on(async {
-            let task1 = loom_executor::spawn(async { 12u128 });
-            //let task2 = loom_executor::spawn(async { 8u128 });
-            task1.await.unwrap() // + task2.await.unwrap()
+        let i: u128 = loom_executor::block_on(async {
+            Delay::default().await;
+            96u128
         });
-        assert_eq!(i, 12);
-        eprintln!("block_on_spawn done!")
+        assert_eq!(i, 96);
     })
 }
 
-#[test]
-fn send_on_closed_channel() {
-    loom::model(|| {
-        let (s, r) = loom::sync::mpsc::channel();
-        std::mem::drop(r);
-        s.send(()).unwrap_err();
-    })
+#[derive(Default)]
+struct Delay {
+    thread_spawned: bool,
+    done: Arc<AtomicBool>,
+}
+
+impl std::future::Future for Delay {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        if !self.thread_spawned {
+            let done = self.done.clone();
+            let waker = cx.waker().clone();
+            thread::spawn(move || {
+                #[cfg(not(loom))]
+                thread::sleep(core::time::Duration::from_millis(100));
+                #[cfg(loom)]
+                thread::yield_now();
+
+                done.store(true, Ordering::SeqCst);
+                waker.wake();
+            });
+            self.thread_spawned = true;
+        }
+        if self.done.load(Ordering::SeqCst) {
+            Poll::Ready(())
+        } else {
+            Poll::Pending
+        }
+    }
 }
